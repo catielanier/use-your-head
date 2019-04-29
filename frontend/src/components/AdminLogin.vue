e5b3cfd95c6b0def37d15363429f9dd3b238384e
Corey Lanier <iam@coreylanier.com>
2019-04-29 16:13:43 -0400

    basic admin form styling

--- frontend/src/components/AdminLogin.vue
+++ frontend/src/components/AdminLogin.vue
@@ -33,18 +33,60 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+    margin: 40px 0 0;
+    padding-bottom: 25px;
+    }
+    form {
+        box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.05);
+        max-width: 640px;
+        margin: 0 auto;
+        background: rgba(0, 0, 0, 0.02);
+        border: 2px solid black;
+        padding: 20px;
+        line-height: 1.5;
+        label {
+            display: block;
+            margin-bottom: 1rem;
+        }
+    }
+    input,
+    textarea,
+    select {
+        width: 90%;
+        padding: 0.5rem;
+        font-size: 1rem;
+        border: 1px solid black;
+        &:focus {
+        outline: 0;
+        border-color: blue;
+        }
+    }
+    button,
+    input[type='submit'] {
+        width: auto;
+        background: red;
+        color: white;
+        border: 0;
+        font-weight: 600;
+        padding: 0.5rem 1.2rem;
+    }
+    fieldset {
+        border: 0;
+        padding: 0;
+
+        &[disabled] {
+        opacity: 0.5;
+        }
+        &::before {
+        height: 10px;
+        content: '';
+        display: block;
+        background-image: linear-gradient(to right, #ff3019 0%, #e2b04a 50%, #ff3019 100%);
+        }
+        &[aria-busy='true']::before {
+        background-size: 50% auto;
+        // animation: ${loading} 0.5s linear infinite;
+        }
+    }
 </style>
