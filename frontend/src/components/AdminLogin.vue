39acae53863dbdc94b9775d5f14b6fe22f5970dd
Corey Lanier <iam@coreylanier.com>
2019-04-29 15:59:01 -0400

    getting rid of default view with admin login panel

---
+++ frontend/src/components/AdminLogin.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="admin">
+        <div class="header">
+            <h1>{{ msg }}</h1>
+        </div>
+        <div class="login">
+            <form action="post">
+                <fieldset>
+                    <h3>Login to the admin panel</h3>
+                    <label for="email">
+                        Email address:
+                        <input type="email" name="email" placeholder="Email address" />
+                    </label>
+                    <label for="password">
+                        Password:
+                        <input type="password" name="password" placeholder="Password">
+                    </label>
+                    <button type="submit">Login</button>
+                </fieldset>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminLogin',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
