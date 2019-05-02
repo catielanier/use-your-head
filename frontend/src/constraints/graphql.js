86adaeb7b6e181e6a2dcdf153cb6870fea853855
Corey Lanier <iam@coreylanier.com>
2019-05-02 16:50:11 -0400

    created front-facing admin creation mutation

--- frontend/src/constraints/graphql.js
+++ frontend/src/constraints/graphql.js
@@ -1,1 +1,21 @@
 import gql from 'graphql-tag';
+
+export const CREATE_ADMIN_MUTATION = gql`
+    mutation CREATE_ADMIN_MUTATION(
+        $email: String!
+        $name: String!
+        $password: String!
+        $verifyPassword: String!
+    ) {
+        createAdmin(
+            email: $email
+            name: $name
+            password: $password
+            verifyPassword: $verifyPassword
+        ) {
+            id
+            email
+            name
+        }
+    }
+`;
