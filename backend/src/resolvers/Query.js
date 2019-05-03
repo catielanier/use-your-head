07ba7d080950f4a84446e3fb982d650a8a71bc59
Corey Lanier <iam@coreylanier.com>
2019-05-03 14:44:40 -0400

    creating query to refetch current user

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,7 +1,17 @@
 const { forwardTo } = require('prisma-binding');
 
 const Query = {
-    admins: forwardTo('db')
+    admins: forwardTo('db'),
+
+    me(parent, args, ctx, info) {
+        // check if current user id
+        if (!ctx.request.userId) {
+            return null;
+        }
+        return ctx.db.query.user({
+            where: { id: ctx.request.userId }
+        }, info);
+    }
 };
 
 module.exports = Query;
