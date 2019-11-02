cbadf551548edec417c7f6e32e7ceb958478dda1
MissingPersonFGC <iam@coreylanier.com>
2019-11-01 23:25:13 -0400

    fixing small issue

--- api/utils/constants.js
+++
@@ -1,5 +0,0 @@
-exports.URL =
-  process.env.MONGODB_URI ||
-  "mongodb+srv://corey:v2BQZiqK4DZMHN7@cluster0-4rbou.mongodb.net/test?retryWrites=true&w=majority";
-exports.PORT = process.env.PORT || 4000;
-exports.SECRET = process.env.SECRET || "alimoandwatereddownshots";
