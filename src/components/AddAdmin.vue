1112db0432b25d7b7806d0d0f8de68afc952ffcf
MissingPersonFGC <iam@coreylanier.com>
2019-11-02 23:42:19 -0400

    creating form for adding admins

---
+++ src/components/AddAdmin.vue
@@ -0,0 +1,108 @@
+<template>
+  <section>
+    <h2 class="display-1">Sign-up</h2>
+    <form
+      @submit.prevent="doSignup"
+      v-bind:disabled="this.loading"
+    >
+      <fieldset v-bind:aria-busy="this.loading">
+        <p
+          v-if="this.error !== null"
+          class="error"
+        >
+          <span>Error:</span> {{this.error}}
+        </p>
+        <label for="username">
+          Name:
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            v-model="username"
+          />
+        </label>
+        <label for="email">
+          Email Address:
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Address"
+            v-model="email"
+          />
+        </label>
+        <label for="password">
+          Password:
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            v-model="password"
+          />
+        </label>
+        <label for="verifyPassword">
+          Verify Password:
+          <input
+            type="password"
+            name="verifyPassword"
+            placeholder="Verify Password"
+            v-model="verifyPassword"
+          />
+        </label>
+        <button type="submit">
+          <font-awesome-icon :icon="['fas', 'user-plus']" /> Sign-up
+        </button>
+      </fieldset>
+    </form>
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+import { setToken } from "../services/tokenService";
+export default {
+  name: "signup",
+  props: {
+    catchUser: Function
+  },
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: "",
+      verifyPassword: "",
+      loading: false,
+      success: false,
+      error: null
+    };
+  },
+  methods: {
+    doSignup: async function() {
+      this.loading = true;
+      const { username, email, password, verifyPassword } = this.$data;
+      if (password !== verifyPassword) {
+        this.error = "Your passwords do not match.";
+        this.loading = false;
+        return;
+      }
+      try {
+        const res = await axios.post("/api/users/signup", {
+          data: {
+            username,
+            email,
+            password
+          }
+        });
+        const { token } = res.data.data;
+        await setToken(token);
+        await this.$props.catchUser();
+        this.loading = false;
+        this.success = true;
+        this.$router.push("/");
+      } catch (e) {
+        this.error = e;
+        this.loading = false;
+      }
+    }
+  }
+};
+</script>
