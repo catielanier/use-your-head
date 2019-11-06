76cb9276f0d8d0a0b3bf4217a153113e17b6fe01
MissingPersonFGC <iam@coreylanier.com>
2019-11-05 23:07:23 -0500

    rendering games to list on page

--- src/components/Games.vue
+++ src/components/Games.vue
@@ -1,6 +1,39 @@
 <template>
   <v-container>
     <h2 class="display-1">Games</h2>
+    <v-container fluid>
+      <v-row id="table-header">
+        <v-col md="5">
+          Game Name
+        </v-col>
+        <v-col md="4">
+          Date Created
+        </v-col>
+        <v-col md="1">
+          Questions
+        </v-col>
+        <v-col md="2">
+          Actions
+        </v-col>
+      </v-row>
+      <v-row
+        v-for="game in games"
+        :key="game._id"
+      >
+        <v-col md="5">
+          {{game.title}}
+        </v-col>
+        <v-col md="4">
+          {{game.dateCreated}}
+        </v-col>
+        <v-col md="1">
+          {{game.questions.length}}
+        </v-col>
+        <v-col md="2">
+
+        </v-col>
+      </v-row>
+    </v-container>
     <v-btn
       color="primary"
       to="/create-game"
@@ -23,3 +56,9 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+#table-header {
+  font-weight: bold;
+}
+</style>
