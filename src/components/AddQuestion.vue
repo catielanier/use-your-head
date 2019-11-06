96c044eabdf0a8f3ece20ad56b15cb531cc528b6
MissingPersonFGC <iam@coreylanier.com>
2019-11-05 21:23:52 -0500

    created forms for adding games and questions

---
+++ src/components/AddQuestion.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-container>
+    <h3 class="display-2">Add a Question:</h3>
+    <v-form @submit.prevent="addQuestion">
+      <v-text-field
+        v-model="question"
+        placeholder="Question"
+      />
+      <v-text-field
+        v-model="expectedAnswer"
+        placeholder="Expected Answer"
+      />
+      <v-text-field
+        v-model="video"
+        placeholder="Video URL"
+      />
+      <v-text-field
+        v-model="image"
+        placeholder="Image URL"
+      />
+      <v-btn
+        type="submit"
+        color="secondary"
+      >Add Question</v-btn>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "add-question",
+  data() {
+    return {
+      question: "",
+      expectedAnswer: "",
+      video: "",
+      image: ""
+    };
+  },
+  props: {
+    pushQuestion: Function
+  },
+  methods: {
+    addQuestion: async function() {
+      const { question, expectedAnswer, video, image } = this.$data;
+      const newQuestion = {
+        question,
+        expectedAnswer,
+        video,
+        image
+      };
+      await this.$props.pushQuestion(newQuestion);
+      this.question = "";
+      this.expectedAnswer = "";
+      this.video = "";
+      this.image = "";
+    }
+  }
+};
+</script>
--- src/components/CreateGame.vue
+++ src/components/CreateGame.vue
@@ -6,12 +6,24 @@
         v-model="title"
         label="Game Title"
       />
-      <h3 class="display-2">Questions:</h3>
+      <h3
+        class="display-2"
+        v-if="questions.length > 0"
+      >Questions:</h3>
+      <v-container
+        v-for="(question, index) in questions"
+        :key="index"
+      >
+        <p>{{question.question}}</p>
+      </v-container>
+      <AddQuestion :pushQuestion="addQuestion" />
+      <v-btn color="primary">Create Game</v-btn>
     </v-form>
   </v-container>
 </template>
 
 <script>
+import AddQuestion from "./AddQuestion";
 export default {
   name: "create-game",
   data() {
@@ -24,6 +36,9 @@ export default {
     addQuestion: function(question) {
       this.questions.push(question);
     }
+  },
+  components: {
+    AddQuestion
   }
 };
 </script>
